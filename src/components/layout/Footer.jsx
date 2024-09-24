5c6753729106ce30355602b61ee94fbb8936ca51
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-09-24 14:02:01 -0300

    feat: primeiro commit com estrutura basica da página

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer() {
+  return (
+    <div className="">
+        <h1 className="text-3xl font-bold underline">
+          Footer!!!
+        </h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Layout() {
+  return (
+    <div className="">
+        <h1 className="text-3xl font-bold underline bg-black text-white">
+          Layout!!!
+        </h1>
+        <h1 className="text-3xl font-bold underline">
+          Layout heheheh!!!
+        </h1>
+    </div>
+  )
+}
+
+export default Layout
