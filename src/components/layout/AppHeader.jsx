99bd5b18d40dbcb6d23fea7be9d0c4794791a688
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-10-19 22:12:46 -0300

    feat: estabelecendo seções da home

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -28,8 +28,8 @@ function AppHeader() {
   };
 
   return (
-    <section className="flex justify-center items-center">
-      <div className="w-full md:w-4/5 h-16 bg-[#01161E] flex flex-row justify-between items-center rounded-b-lg px-4 md:px-16">
+    <section className="fixed top-0 left-0 right-0 z-50 flex justify-center items-center">
+      <div className="w-full md:w-4/5 h-16 bg-[#01161E] flex flex-row justify-between items-center md:rounded-b-lg rounded-none px-4 md:px-16">
         
         {/* Logo */}
         <img src={Logo} alt="logo" className="h-1/3 md:h-1/3" />
