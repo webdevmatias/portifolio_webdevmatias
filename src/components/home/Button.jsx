c77f7b81e1467e6a8074c25e7f78a04a410f07bc
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-10-21 21:23:25 -0300

    feat: section home pronta e respondiva

--- src/components/home/Button.jsx
+++ src/components/home/Button.jsx
@@ -1,14 +1,27 @@
 // Button.js
 import React from "react";
 
-const Button = ({ icon, label }) => {
-  return (
+const Button = ({ icon, label, clickable = true, color = "#FB8500", link = "#" }) => {
+  const buttonContent = (
     <div 
-      className="flex flex-row gap-2 items-center px-4 py-2 my-2 border border-[#FB8500] text-[#FB8500] rounded-lg shadow-md transition-colors"
+      className={`flex flex-row gap-2 justify-center items-center px-4 py-1 my-2 border rounded-md shadow-md transition-colors ${clickable ? 'cursor-pointer' : 'cursor-default'}`}
+      style={{
+        borderColor: color,    // Borda na cor personalizada
+        color: color,          // Texto e ícone na cor personalizada
+        opacity: clickable ? 1 : 0.8, // Menor opacidade para não-clicáveis
+      }}
     >
       {icon}
       <span className="text-sm md:text-base">{label}</span>
     </div>
+  );
+
+  return clickable ? (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      {buttonContent}
+    </a>
+  ) : (
+    buttonContent
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,67 +1,46 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaReact, FaNodeJs, FaJsSquare } from 'react-icons/fa'; // Ícones do React, Node.js, JS
-import { SiTailwindcss } from 'react-icons/si'; // Ícone do TailwindCSS
-import { MdDesignServices } from 'react-icons/md'; // Ícone para UI/UX Design
+import { FaLinkedin, FaGithub } from 'react-icons/fa'; // Ícones do LinkedIn e GitHub
 import Button from '../components/home/Button.jsx'; // Importa o componente de botão
+import MatiasPic from '../../public/matias_pic.svg'
 
 const Home = () => {
   return (
     <section className="flex justify-center items-center">
       <div className="flex flex-col justify-center items-center bg-black w-full md:w-4/5 min-h-screen">
-        <div className="flex flex-col-reverse md:flex-row justify-between items-center w-full px-4 md:px-16">
+        {/* Seção main */}
+        <div className="flex flex-col-reverse md:flex-row justify-between items-center w-full px-16 md:px-40">
           {/* Seção de texto */}
           <div className="flex flex-col text-white w-full md:w-2/3 text-center md:text-left">
-            <h1 className="raleway-800 text-3xl md:text-4xl lg:text-5xl">Olá, eu sou o</h1>
-            <h1 className="raleway-800 text-5xl md:text-6xl lg:text-7xl font-bold text-[#FB8500]">MATIAS :D</h1>
+            <h1 className="ibm-font text-3xl md:text-4xl lg:text-5xl">Olá, eu sou o</h1>
+            <h1 className="ibm-font text-5xl md:text-6xl lg:text-7xl font-bold text-[#FB8500]">MATIAS!</h1>
             <h2 className="ibm-plex-mono-regular text-sm md:text-base lg:text-lg">
               Desenvolvedor Front-End
             </h2>
-            <div className="flex flex-row justify-center md:justify-start mt-4 space-x-4">
-              {/* Botão LinkedIn Não Clicável */}
+            <div className="flex flex-col md:flex-row justify-center md:gap-4 md:justify-start mt-4">
+              {/* Botão LinkedIn Clicável */}
               <Button 
                 icon={<FaLinkedin size={18} className="md:text-2xl" />} 
                 label="LinkedIn"
+                clickable={true}       // Botão clicável
+                color="#FB8500"        // Define a cor do botão
+                link="https://www.linkedin.com/in/matiaz/" // Link para LinkedIn
               />
-              {/* Botão GitHub Não Clicável */}
+              {/* Botão GitHub Clicável */}
               <Button 
                 icon={<FaGithub size={18} className="md:text-2xl" />} 
                 label="GitHub"
+                clickable={true}       // Botão clicável
+                color="#FB8500"        // Define a cor do botão
+                link="https://github.com/webdevmatias" // Link para GitHub
               />
             </div>
           </div>
           {/* Seção de imagem */}
-          <div className="mt-8 md:mt-0 w-full md:w-1/3 flex justify-center py-2">
+          <div className="mt-8 mb-4 md:mt-0 w-full md:w-1/3 flex justify-center py-2">
             <img 
-              src="caminho/para/sua/imagem.jpg" 
+              src={MatiasPic} 
               alt="Foto do Matias" 
               className="rounded-full w-40 h-40 md:w-60 md:h-60 object-cover bg-yellow-500"
-            />
-          </div>
-        </div>
-        {/* Seção de skills */}
-        <div className="flex flex-col items-center text-white mt-8">
-          <h2 className="raleway-800 text-2xl md:text-3xl mb-4">Principais Skills:</h2>
-          <div className="flex flex-wrap justify-center space-x-4 mb-16">
-            {/* Botões de Skills Não Clicáveis */}
-            <Button 
-              icon={<FaReact size={18} className="md:text-2xl" />} 
-              label="React"
-            />
-            <Button 
-              icon={<FaNodeJs size={18} className="md:text-2xl" />} 
-              label="Node.js"
-            />
-            <Button 
-              icon={<FaJsSquare size={18} className="md:text-2xl" />} 
-              label="JavaScript"
-            />
-            <Button 
-              icon={<SiTailwindcss size={18} className="md:text-2xl" />} 
-              label="TailwindCSS"
-            />
-            <Button 
-              icon={<MdDesignServices size={18} className="md:text-2xl" />} 
-              label="UI/UX Design"
             />
           </div>
         </div>
